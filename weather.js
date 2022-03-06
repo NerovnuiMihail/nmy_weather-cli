ac1329e153ed05097fce0c00cd725985308febe8
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-03-06 13:37:41 +0300

    complete

---
+++ weather.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+import { getWeather, getIcon } from "./services/api.service.js";
+import { printHelp, printSuccess, printError, printWeather } from "./services/log_services.js";
+import { saveKeyValue, getKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+
+
+const saveToken = async (token) => {
+    if(!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Токен сохранен');
+    } catch(e) {
+        printError(e.message);
+    }
+};
+
+const saveCity = async (city) => {
+    if(!city.length) {
+        printError('Не передан город');
+        return;
+    }
+
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('Город сохранен');
+    } catch(e) {
+        printError(e.message);
+    }
+};
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        printWeather(weather,getIcon(weather.weather[0].icon)) // вывод погоды
+    } catch(e) {
+        if (e?.response?.status == 404) {
+            printError('Неверно указан город');
+        } else if (e?.response?.status == 401) {
+            printError('Неверно указан токен');
+        } else {
+            printError(e.message);
+        }
+    }
+};
+
+const initCLI = () => {
+    const args = getArgs(process.argv);
+    
+    if ( args.h ) {
+        //вывод help
+        return printHelp();
+    }
+
+    if ( args.s ) {
+        //Сохранить город
+        return saveCity(args.s);
+    }
+
+    if ( args.t ) {
+        //Сохранить токен
+        return saveToken(args.t);
+    }
+    //Вывести погоду
+    return getForcast();
+};
+
+initCLI();
